3b1640afcc88b0a2afc85b55bc6e1d9979e57446
raul2020 <raul_2020_@hotmail.com>
2018-02-15 22:36:01 -0800

    Add files via upload

---
+++ Nosotros.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Nosotros</title>
+		<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Donaciones Prueba</title>
+
+    <!-- Latest compiled and minified CSS https://www.fundacionalbihar.org/-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" type="text/css" href="mystyle.css">
+  </head>
+	</head>
+	<body background="imagenes/background/images (5).jpg">
+	<header>
+          <nav class="navbar navbar-default">
+          <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Asociacion</a>
+              </div>
+
+               <ul class="nav navbar-nav navbar-right">
+                  <li role="presentation" ><a href="index.html">Bienvenido</a></li>
+                  <li role="presentation" class="active"><a href="nosotros.php">Nosotros</a></li>
+                  <li role="presentation" ><a href="#1">Que Hacemos</a></li>
+                  <li role="presentation" ><a href="#2">Contactanos</a></li>
+               </ul>
+          </nav>
+    </header>
+    <div class="container" style="background-color:#ccff99;">	
+    		<div class="row">
+    			<div class="col-md-12">
+					<strong><h1 align="center">Nosotros</h1></strong>	
+				</div>
+			</div>
+    	<section>
+			<div class="row">
+				<div class="col-md-12">
+					<h2><p>VISIÓN</p></h2>
+					<div align="center">
+					<img src="imagenes/vision.png" width="50%">
+					</div>	
+					<h3>			
+					<p>Buscamos ser una Corporación consolidada y activa en los procesos sociales atreves de brindar servicio social y humanitario para las familias vulnerables, liderando proyectos que involucren a toda la comunidad especialmente a entidades sociales, con el fin de asegurar el bienestar social, económico y educativo, enfocando proyectos que mejore la calidad de vida y un cambio para las comunidades, logrando que la sociedad sea más comprometida al cambio y más participativa para crear nuevos ambientes de convivencia que permita la consolidación de una comunidad bajo valores y principios de solidaridad.</p>
+					</h3>
+				</div>			
+			</div>
+		</section>
+		<hr>
+		<section>
+			<div class="row">
+				<div class="col-md-12">
+					<h2><p>VALORES</p></h2>
+					<div align="center">
+					<img src="imagenes/valores.png" width="50%">
+					</div>	
+					<h3>
+					 	<p>Contamos con valores éticos y morales para llevar acabo nuestros objetivos.</p>
+						<p>Compromiso: con toda comunidad para llevar acabo cada proyecto que emprendamos.</p>
+						<p>Responsabilidad social: preocuparnos por el bienestar de cada una de las personas integradas a nuestra fundación.</p>
+						<p>Respeto y dignidad: velar por el respeto y el buen trato hacia la comunidad.</p>
+						<p>Trabajo en equipo: trabajaremos juntos para ayudar a los que nos necesiten.</p>
+						<p>Trabajo con la misma comunidad: Hacemos participe a la comunidad de los proyectos, para que se apropie y se beneficien mutuamente, mejorando su autonomía y sus redes comunitarias.</p>
+						<p>Respeto por la cultura: en cada uno de los proyectos se tendrá en cuenta el valor cultural, porque ningún proyecto violara la cultura y la costumbre de los pueblos.</p>
+						<p>Respeto por la autonomía: Cada uno de los pueblos tienen su autonomía, por eso cada proyecto será pasado por las leyes autonómicas de la comunidad.</p>
+					</h3>
+					
+				</div>
+			</div>
+		</section>
+		<hr>
+		<section>
+			<div class="row">
+				<div class="col-md-12">
+				<h2><p>PROYECTOS</p></h2>
+				<h3>
+					<p>-Discapacidad con autonomía.</p>
+					<p>-Semillas con amor.</p>
+					<p>-Hogar de amor.</p>
+					<p>-Pequeños felices.</p>
+					<p>-La guajira.</p>
+					<div class="row">
+						<div class="col-md-12">
+							<div class="proyecto">
+							<strong><p>Brigadas de Salud</p></strong>
+							<p>Realizamos una caracterización de las familias con personas con movilidad reducida, que son de escasos recursos y olvidados por el estado, la Fundación Pro-Futuro Medellín les lleva la silla de ruedas. Trabajamos por ellos, son nuestro impulso, para cada día ayudar a los más necesitados, la recompensa que recibimos son las sonrisas de alegría, amor y esperanza en su mirada lo dicen todo. Este proyecto que empezó como un sueño y ahora aunque con mucho esfuerzo ya es una realidad.</p>
+							</div>
+						</div>
+					</div>
+					<strong><p>SEMILLAS CON AMOR</p></strong>
+					<p>Lideramos proyectos de bienestar social y humanitario, en el compromiso que la Fundación pro-Futuro Medellín, adquirió un compromiso con las familias de las personas que se encuentran privados de su libertad y con alto grado de vulnerabilidad, cumplimos mensualmente con mercados para las familias beneficiadas, les brindamos una mejor alimentación y generamos influencia positiva en ellos.</p>
+
+					<strong><p>HOGAR DE AMOR</p></strong>
+					<p>Deseamos liderar el proyectos HOGAR CON AMOR, de bienestar social y humanitario es un
+					compromiso que la Fundación pro-Futuro Medellín a tomado y por eso, adquirirá una casa hogar para recibir por seis meses a las mujeres que están en estado de embarazo y privadas de su libertad, se les otorgara seguimiento medico y psicológico con cómodas instalaciones generando tener un habiente de tranquilidad y aprendizaje continuamente.</p>
+
+					<strong><p>PEQUEÑOS FELICES</p></strong>
+					<p>La Fundación Pro-Futuro Medellín, se preocupa por mejorar la calidad de vida de los niños, teniendo presente que todos merecemos condiciones humanas para una mejor calidad de vida y educación, brindándoles la oportunidad e una vida con mas oportunidades a los hijos de las personas quien se encuentran privadas de su libertad, mes a mes lo que son Pañales, ropa, útiles de aseo, gracias a la colaboración de todos ustedes hacemos realidad toda y podemos seguir con esta noble causa y seguimos con nuestra labor.</p>
+
+					<strong><p>LA GUAJIRA</p></strong>
+					<p>Siempre pensando en la función de gestionar para las personas mas vulnerables, llegamos hasta la zona de
+					la guajira apoyando con nuestro equipo de trabajo, poniendo día a día lo mejor de nosotros en nuestras labores sociales.</p>
+
+					<strong><p>Brigada de Salud en Corregimientos</p></strong>
+					<p>Se realizaron unas Brigadas de Salud en Los corregimientos de; Terraplen, Puerto Oculto y El Diviso en San Martin (cesar).</p>
+				</h3>
+				</div>
+			</div>	
+		</section>
+	</div>
+	</body>
+	</html>	
